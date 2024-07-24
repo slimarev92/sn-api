6f4fc1acdc530505e0e1cca03ae34fed7868b94b
Sasha Limarev <slimarev@outlook.com>
2024-07-24 17:51:14 +0300

    Removed unused file

--- app/src/components/users/users-contexts.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { IUsersAction, IUsersState } from "./UsersService";
-
-export const DispatchUsersAction = createContext<React.Dispatch<IUsersAction>>(() => {});
-
-export const UsersContext = createContext<IUsersState>({
-    loading: false,
-    users: [],
-});
